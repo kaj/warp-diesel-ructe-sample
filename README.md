1b4a9c14fad3dcf9738caca7f919e60da39ac652
Rasmus Kaj <kaj@kth.se>
2018-08-18 10:43:27 +0200

    Initial checkin: warp + templates.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Web example: Login with warp, ructe, and diesel
+
+This application is intended as an example of handling a login
+session, using the warp web framework, the ructe template engine and
+the diesel database layer.
---
+++ src/main.rs
@@ -0,0 +1,119 @@
+//! An example web service using ructe with the warp framework.
+#![deny(warnings)]
+extern crate env_logger;
+extern crate mime;
+extern crate warp;
+
+mod render_ructe;
+
+use render_ructe::RenderRucte;
+use std::io::{self, Write};
+use std::time::{Duration, SystemTime};
+use templates::statics::StaticFile;
+use warp::http::{Response, StatusCode};
+use warp::{path, reject, Filter, Rejection, Reply};
+
+/// Main program: Set up routes and start server.
+fn main() {
+    env_logger::init();
+
+    let login_routes =
+        path("login").and(warp::index())
+        .and(
+            warp::get2().and_then(login_form)
+                .or(warp::post2().and_then(do_login))
+        );
+            
+    let routes = warp::get2()
+        .and(
+            warp::index()
+                .and_then(home_page)
+                .or(path("static").and(path::param()).and_then(static_file))
+                .or(path("bad").and_then(bad_handler)),
+        ).or(login_routes)
+        .recover(customize_error);
+    warp::serve(routes).run(([127, 0, 0, 1], 3030));
+}
+
+fn login_form() -> Result<impl Reply, Rejection> {
+    Response::builder().html(|o| {
+        templates::login(o, None, None)
+    })
+}
+
+fn do_login() -> Result<impl Reply, Rejection> {
+    Response::builder().html(|o| {
+        templates::login(o, None, None)
+    })
+}
+
+
+/// Home page handler; just render a template with some arguments.
+fn home_page() -> Result<impl Reply, Rejection> {
+    Response::builder().html(|o| {
+        templates::page(o, &[("first", 3), ("second", 7), ("third", 2)])
+    })
+}
+
+/// A handler that always gives a server error.
+fn bad_handler() -> Result<StatusCode, Rejection> {
+    Err(reject::server_error())
+}
+
+/// This method can be used as a "template tag", i.e. a method that
+/// can be called directly from a template.
+fn footer(out: &mut Write) -> io::Result<()> {
+    templates::footer(
+        out,
+        &[
+            ("ructe", "https://crates.io/crates/ructe"),
+            ("warp", "https://crates.io/crates/warp"),
+        ],
+    )
+}
+
+/// Handler for static files.
+/// Create a response from the file data with a correct content type
+/// and a far expires header (or a 404 if the file does not exist).
+fn static_file(name: String) -> Result<impl Reply, Rejection> {
+    if let Some(data) = StaticFile::get(&name) {
+        let _far_expires = SystemTime::now() + FAR;
+        Ok(Response::builder()
+            .status(StatusCode::OK)
+            .header("content-type", data.mime.as_ref())
+            // TODO .header("expires", _far_expires)
+            .body(data.content))
+    } else {
+        println!("Static file {} not found", name);
+        Err(reject::not_found())
+    }
+}
+
+/// A duration to add to current time for a far expires header.
+static FAR: Duration = Duration::from_secs(180 * 24 * 60 * 60);
+
+/// Create custom error pages.
+fn customize_error(err: Rejection) -> Result<impl Reply, Rejection> {
+    match err.status() {
+        StatusCode::NOT_FOUND => {
+            eprintln!("Got a 404: {:?}", err);
+            // We have a custom 404 page!
+            Response::builder().status(StatusCode::NOT_FOUND).html(|o| {
+                templates::error(
+                    o,
+                    StatusCode::NOT_FOUND,
+                    "The resource you requested could not be located.",
+                )
+            })
+        }
+        code => {
+            eprintln!("Got a {}: {:?}", code.as_u16(), err);
+            Response::builder()
+                .status(code)
+                .html(|o| templates::error(o, code, "Something went wrong."))
+        }
+    }
+}
+
+// And finally, include the generated code for templates and static files.
+include!(concat!(env!("OUT_DIR"), "/templates.rs"));
