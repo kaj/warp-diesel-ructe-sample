e8fd3b0cdb3b4e9d4665da85aebe69bad6907fdc
Rasmus Kaj <kaj@kth.se>
2018-08-19 19:27:38 +0200

    Implement ToHtml for User.

--- src/models.rs
+++ src/models.rs
@@ -1,8 +1,23 @@
 use schema::users;
+use std::io::{self, Write};
+use templates::ToHtml;
 
 #[derive(Debug, Identifiable, Queryable)]
 pub struct User {
     pub id: i32,
     pub username: String,
     pub realname: String,
+}
+
+// Implementing ToHtml for a type makes it possible to use that type
+// directly in templates.
+impl ToHtml for User {
+    fn to_html(&self, out: &mut Write) -> io::Result<()> {
+        out.write_all(b"<span data-user=\"")?;
+        self.username.to_html(out)?;
+        out.write_all(b"\">")?;
+        self.realname.to_html(out)?;
+        out.write_all(b"</span>")?;
+        Ok(())
+    }
 }
